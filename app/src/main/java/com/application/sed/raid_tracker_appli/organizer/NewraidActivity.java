f3ba341d48418a4581582e836bf0780deeb7250a
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-10-08 12:52:09 +0200

    Del ActionBar

--- app/src/main/java/com/application/sed/raid_tracker_appli/organizer/NewraidActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/organizer/NewraidActivity.java
@@ -3,7 +3,10 @@ package com.application.sed.raid_tracker_appli.organizer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.application.sed.raid_tracker_appli.R;
 import com.application.sed.raid_tracker_appli.Utils;
 
 public class NewraidActivity extends AppCompatActivity {
@@ -12,6 +15,8 @@ public class NewraidActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_newraid);
         Utils.info(TAG,"Creation of the new activity");
     }
 }
