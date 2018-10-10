39f6eb0d9908013a0983bac8f2bdf655bd761d0a
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-10-10 10:14:43 +0200

    Refactor : Landing into Master

--- app/src/main/java/com/application/sed/raid_tracker_appli/ConnexionActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/ConnexionActivity.java
@@ -27,7 +27,7 @@ public class ConnexionActivity extends AppCompatActivity {
 
         // Test du bouton avec le user toto et password test
         if (user.getText().toString().equals("Username") & pass.getText().toString().equals("Password")){
-            Utils.debug(TAG, "coool");
+            Utils.debug(TAG, "cool");
             Intent intent = new Intent(ConnexionActivity.this, NewraidActivity.class);
             Utils.info(TAG,"connexion, new activity");
             startActivity(intent);
