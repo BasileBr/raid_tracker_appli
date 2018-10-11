8b27256ab0e5cb724262e5fea5643650a10f12f4
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-10-11 16:21:16 +0200

    Feat : add Hamburger Menu

--- app/src/main/java/com/application/sed/raid_tracker_appli/LandingActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/LandingActivity.java
@@ -1,38 +1,86 @@
 package com.application.sed.raid_tracker_appli;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.application.sed.raid_tracker_appli.organizer.NewraidActivity;
 
 
 public class LandingActivity extends AppCompatActivity {
 
     private String TAG = "WelcomeActivity";
     private DrawerLayout drawerLayout;
+
+    private Toolbar toolbar;
+    View button;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         Utils.info(TAG, "OnCreate");
 
-        drawerLayout = findViewById(R.id.drawerLayout);
-        final TextView user = findViewById(R.id.username);
+        this.toolbar = findViewById(R.id.toolbar);
+
+        //definir notre toolbar en tant qu'actionBar
+        setSupportActionBar(toolbar);
+
+        //afficher le bouton retour
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        this.drawerLayout = findViewById(R.id.drawerLayout);
+        this.drawerToggle = new ActionBarDrawerToggle(this,this.drawerLayout,0,0);
+        this.drawerLayout.setDrawerListener(this.drawerToggle);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.info("NICK", "button button button..................");
+
+                drawerLayout.openDrawer(Gravity.LEFT);
+
+            }
+        });
+
+
+
+
+
+        /*drawerLayout = findViewById(R.id.drawerLayout);
+        final TextView user = findViewById(R.id.username);*/
         //user.setText("Bienvenue " +Utils.Name);
     }
+
+
+    //ne pas oublier de copier/coller ces 2 méthodes
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // synchroniser le drawerToggle après la restauration via onRestoreInstanceState
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
     public void join(View view) {
 
     }
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_landing, menu);
@@ -52,5 +100,5 @@ public class LandingActivity extends AppCompatActivity {
             default:
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
