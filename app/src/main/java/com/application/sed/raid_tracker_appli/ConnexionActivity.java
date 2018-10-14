f7e7b080bb8c152ee6d00847392e3d8e2c35a122
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-10-14 12:03:42 +0200

    Fix : Menu hamburger " rejoins-nous"

--- app/src/main/java/com/application/sed/raid_tracker_appli/ConnexionActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/ConnexionActivity.java
@@ -1,8 +1,13 @@
 package com.application.sed.raid_tracker_appli;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,14 +17,59 @@ import com.application.sed.raid_tracker_appli.organizer.NewraidActivity;
 public class ConnexionActivity extends AppCompatActivity {
     private String TAG = "ConnexionActivity";
 
+    private DrawerLayout drawerLayout;
+
+    private Toolbar toolbar2;
+    View button;
+    private ActionBarDrawerToggle drawerToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
         Utils.warm(TAG, "OnCreate");
 
+        this.toolbar2 = findViewById(R.id.toolbar2);
+
+        //definir notre toolbar en tant qu'actionBar
+        setSupportActionBar(toolbar2);
+
+        //afficher le bouton retour
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        this.drawerLayout = findViewById(R.id.drawerLayout2);
+        this.drawerToggle = new ActionBarDrawerToggle(this,this.drawerLayout,0,0);
+        this.drawerLayout.setDrawerListener(this.drawerToggle);
+
+        toolbar2.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.info("NICK", "button button button..................");
+
+                drawerLayout.openDrawer(Gravity.LEFT);
+
+            }
+        });
 
     }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // synchroniser le drawerToggle après la restauration via onRestoreInstanceState
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
 
     public void login_connexion(View view){
         final EditText user = findViewById(R.id.username);
