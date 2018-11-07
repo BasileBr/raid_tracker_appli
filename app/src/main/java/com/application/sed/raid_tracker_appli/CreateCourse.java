0d9313240437548d23333e9efd9e5c5e5151f9f7
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-11-07 21:55:18 +0100

    Update : CourseActivity

--- app/src/main/java/com/application/sed/raid_tracker_appli/CreateCourse.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/CreateCourse.java
@@ -24,6 +24,7 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.application.sed.raid_tracker_appli.organizer.CourseActivity;
 import com.application.sed.raid_tracker_appli.organizer.NewraidActivity;
 
 import java.util.ArrayList;
@@ -649,14 +650,14 @@ public class CreateCourse extends AppCompatActivity {
             LandingActivity.recupereraid();
             Utils.info("EditText", Bdd.getElement(1).toString());
 
-//            //déclencher la création d'un parcours sur la page de landing
-//                        Intent intent = new Intent(CreateCourse.this, LandingActivity.class);
-//                        intent.putExtra("name","Toto");
-//                        startActivity(intent);
-//
-//            //redirection vers la page de création d'un parcours
-//                        Intent intent2= new Intent(CreateCourse.this, CreateParcours.class);
-//                        startActivity(intent2);
+            //déclencher la création d'un parcours sur la page de landing
+                        Intent intent = new Intent(CreateCourse.this, LandingActivity.class);
+                        intent.putExtra("name","Toto");
+                        startActivity(intent);
+
+            //redirection vers la page de création d'un parcours
+                        Intent intent2= new Intent(CreateCourse.this, CourseActivity.class);
+                        startActivity(intent2);
         }
 
 
