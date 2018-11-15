45af89416fb3a28a95d1fa406c62e94a3ad23a92
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-11-15 22:45:22 +0100

    Update : Connexion + récup raid + delete token + create raid

--- app/src/main/java/com/application/sed/raid_tracker_appli/API/ApiRequestPost.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/API/ApiRequestPost.java
@@ -8,11 +8,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.application.sed.raid_tracker_appli.Accueil;
 import com.application.sed.raid_tracker_appli.Utils.Bdd;
 import com.application.sed.raid_tracker_appli.Utils.Utils;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -20,6 +22,8 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.internal.Util;
 
 public class ApiRequestPost {
 
@@ -117,15 +121,25 @@ public class ApiRequestPost {
     public static void postRaid(Context context, final String token, final String name, final String lieu, final String date, final String edition, final String equipe){
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        StringRequest postRequest = new StringRequest(Request.Method.POST, urlRaid,
+        JsonObject params = new JsonObject();
+        JsonParser parser = new JsonParser();
+        Utils.debug("CreateRaid", "nom " + name+" lieu " + lieu+ " date " + date+" edition  " + edition+ " equipe" +equipe);
+        params.add("nom",parser.parse(name));
+        params.add("lieu",parser.parse(lieu);
+        params.add("date",parser.parse(date);
+        params.add("edition",parser.parse(edition));
+        params.add("equipe", parser.parse(equipe);
+
+        Utils.debug("CreateRaid",params.toString());
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, urlRaid,
                 new Response.Listener<String>()
-                {
+                {   
                     @Override
                     public void onResponse(String response) {
                         // response
 
 
-                        Log.d("Response", response);
+                        Log.d("Response creation raid", response);
 
                         Accueil.redirection(response);
                     }
@@ -145,25 +159,28 @@ public class ApiRequestPost {
                 Map<String, String> header = new HashMap<>();
                 String auth;
                 Utils.debug("Header",token);
-                header.put("Content-Type", "application/json");
+                //header.put("Content-Type", "application/json");
                 header.put("X-Auth-Token",token);
                 return header;
             }
 
-            @Override
-            protected Map<String, String> getParams()
-            {
+//            @Override
+//            protected Map<String, String> getParams()
+//            {
+//
+//                Map<String, String>  params = new HashMap<>();
+//                Utils.debug("CreateRaid", "nom " + name+" lieu " + lieu+ " date " + date+" edition  " + edition+ " equipe" +equipe);
+//                params.put("nom",name);
+//                params.put("lieu",lieu);
+//                params.put("date",date);
+//                params.put("edition",edition);
+//                params.put("equipe", equipe);
+//
+//                Utils.debug("CreateRaid",params.toString());
+//                return params;
+//            }
 
-                Map<String, String>  params = new HashMap<>();
-                params.put("nom",name);
-                params.put("lieu",lieu);
-                params.put("date",date);
-                params.put("edition",edition);
-                params.put("equipe", equipe);
 
-
-                return params;
-            }
         };
         requestQueue.add(postRequest);
 
