6890af1ba04b5c3bfc5a5b2a4f3be3bd5c02b4a9
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-12-04 22:57:02 +0100

    Update : recup info poste

--- app/src/main/java/com/application/sed/raid_tracker_appli/Utils/Utils.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/Utils/Utils.java
@@ -14,23 +14,23 @@ public class Utils {
 
     public static String Name;
 
-    public static void info(String TAG, String log){
+    public static void info(String TAG, String log) {
         Log.i(TAG, log);
     }
 
-    public static void warm(String TAG, String log){
+    public static void warm(String TAG, String log) {
         Log.w(TAG, log);
     }
 
-    public static void err(String TAG, String log){
+    public static void err(String TAG, String log) {
         Log.e(TAG, log);
     }
 
-    public static void debug(String TAG, String log){
+    public static void debug(String TAG, String log) {
         Log.d(TAG, log);
     }
 
-    public static void Permission(Activity context){
+    public static void Permissionlocation(Activity context) {
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -56,6 +56,9 @@ public class Utils {
             // Permission has already been granted
         }
 
+    }
+
+    public static void Permissionexternalstorage(Activity context) {
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -81,6 +84,9 @@ public class Utils {
             // Permission has already been granted
         }
 
+    }
+
+    public static void Permissionreadexternalstorage(Activity context) {
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -105,32 +111,9 @@ public class Utils {
         } else {
             // Permission has already been granted
         }
+    }
 
-
-        if (ContextCompat.checkSelfPermission(context,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(context,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        1);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
+    public static void Permissioncoarselocation(Activity context) {
 
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -156,32 +139,9 @@ public class Utils {
         } else {
             // Permission has already been granted
         }
+    }
 
-        if (ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_WIFI_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
-                    Manifest.permission.ACCESS_WIFI_STATE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(context,
-                        new String[]{Manifest.permission.ACCESS_WIFI_STATE},
-                        1);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
-
+    public static void Permissionnetworkstate(Activity context) {
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_NETWORK_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
--- app/src/main/java/com/application/sed/raid_tracker_appli/helper/EditPosteActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/helper/EditPosteActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.application.sed.raid_tracker_appli.API.ApiRequestGet;
 import com.application.sed.raid_tracker_appli.LandingActivity;
@@ -22,22 +25,38 @@ public class EditPosteActivity extends AppCompatActivity {
 
     public static Context context;
     private Toolbar toolbar;
-    public String idRaid;
+    public static String idRaid;
 
+    public static TextView nomposte;
     public static EditText nomposteentry;
+    public static TextView nombre;
     public static EditText nombreentry;
 
+    public static TextView debut;
     public static EditText anneedebut;
     public static EditText moisdebut;
     public static EditText joursdebut;
     public static EditText heuredebut;
     public static EditText minutedebut;
 
+    public static TextView fin;
     public static EditText anneefin;
     public static EditText moisfin;
     public static EditText joursfin;
     public static EditText heurefin;
     public static EditText minutefin;
+
+    public static Button valider;
+    public static Button annuler;
+
+    public static TextView mission;
+    public static EditText missionEntry;
+
+    public static int boolnom;
+    public static int boolnombre;
+    public static int booldebut;
+    public static int boolfin;
+    public static int boolmission;
 
 
     @Override
@@ -76,20 +95,41 @@ public class EditPosteActivity extends AppCompatActivity {
             Utils.debug("EditPosteActicity", String.valueOf(context.toString().contains("com.application.sed.raid_tracker_appli.helper.EditPosteActivity")));
 
 
+            nomposte = findViewById(R.id.nomposte);
             nomposteentry = findViewById(R.id.nomposteentry);
+            nombre = findViewById(R.id.nombre);
             nombreentry = findViewById(R.id.nombreentry);
 
+            debut = findViewById(R.id.debut);
             anneedebut = findViewById(R.id.anneedebut);
             moisdebut = findViewById(R.id.moisdebut);
             joursdebut = findViewById(R.id.joursdebut);
             heuredebut = findViewById(R.id.heuredebut);
             minutedebut = findViewById(R.id.minutedebut);
 
+            fin = findViewById(R.id.fin);
             anneefin = findViewById(R.id.anneefin);
             moisfin = findViewById(R.id.moisfin);
             joursfin = findViewById(R.id.joursfin);
             heurefin = findViewById(R.id.heurefin);
             minutefin = findViewById(R.id.minutefin);
+
+            valider = findViewById(R.id.valider);
+            annuler = findViewById(R.id.annuler);
+
+            mission = findViewById(R.id.mission);
+            missionEntry = findViewById(R.id.missionentry);
+
+            annuler.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ManageVolunteersPositionActivity.class);
+                    intent.putExtra("idRaid",idRaid);
+                    context.startActivity(intent);
+                }
+            });
+
+
             ApiRequestGet.getOnePoste(context, Bdd.getValue(), idPoste);
         }
     }
@@ -106,6 +146,56 @@ public class EditPosteActivity extends AppCompatActivity {
         nomposteentry.setText(jsonObject.get("type").toString().replace("\"",""));
         nombreentry.setText(jsonObject.get("nombre").toString());
 
+        anneedebut.setText(jsonObject.get("heureDebut").toString().substring(1,5));
+        moisdebut.setText(jsonObject.get("heureDebut").toString().substring(6,8));
+        joursdebut.setText(jsonObject.get("heureDebut").toString().substring(9,11));
+        heuredebut.setText(jsonObject.get("heureDebut").toString().substring(12,14));
+        minutedebut.setText(jsonObject.get("heureDebut").toString().substring(15,17));
 
+        anneefin.setText(jsonObject.get("heureFin").toString().substring(1,5));
+        moisfin.setText(jsonObject.get("heureFin").toString().substring(6,8));
+        joursfin.setText(jsonObject.get("heureFin").toString().substring(9,11));
+        heurefin.setText(jsonObject.get("heureFin").toString().substring(12,14));
+        minutefin.setText(jsonObject.get("heureFin").toString().substring(15,17));
+
+
+        Utils.debug("heuredebut",jsonObject.get("heureDebut").toString());
+        Utils.debug("heurefin",jsonObject.get("heureFin").toString());
+
+        if (isEmpty(nomposteentry)){
+            nomposte.setError("Le champ est vite");
+        }
+        if (isEmpty(nombreentry)){
+            nombre.setError("Le champ est vite");
+        }
+        if ( (isEmpty(anneedebut)) || (isEmpty(moisdebut)) || (isEmpty(joursdebut)) || (isEmpty(heuredebut)) || (isEmpty(minutedebut)) ){
+            debut.setError("Le champ est vite");
+        }
+        if ( (isEmpty(anneefin)) || (isEmpty(moisfin)) || (isEmpty(joursfin)) || (isEmpty(heurefin)) || (isEmpty(minutefin)) ){
+            fin.setError("Le champ est vite");
+        }
+
+        if (isEmpty(missionEntry)){
+            mission.setError("Le champ est vite");
+        }
+
+
+    }
+
+    public static void updateinfo(){
+
+
+
+        valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public static boolean isEmpty (EditText text){
+        CharSequence str= text.getText().toString();
+        return TextUtils.isEmpty(str);
     }
 }
