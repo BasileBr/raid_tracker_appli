261651e160e7364e8c5ee8b4208fe5d3d3d5057b
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-12-05 19:00:10 +0100

    Fix : Creation Parcours

--- app/src/main/java/com/application/sed/raid_tracker_appli/organizer/CreateParcours.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/organizer/CreateParcours.java
@@ -322,6 +322,7 @@ public class CreateParcours extends AppCompatActivity implements MapEventsReceiv
 
     public static void trace(String id){
         idTrace = id;
+        Utils.debug("TAG",idTrace);
 
     }
     //initialisation de compteur pour tester si on peut ajouter un nouveau drapeau
