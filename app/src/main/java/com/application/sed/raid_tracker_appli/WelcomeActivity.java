ff429ac44e19e25dce000c6a584fa88089076946
yvan ulas <yulas@enssat.fr>
2019-01-19 19:07:32 +0100

    Update : Affichage des raids dont la date n'est pas dépassée

--- app/src/main/java/com/application/sed/raid_tracker_appli/WelcomeActivity.java
+++ app/src/main/java/com/application/sed/raid_tracker_appli/WelcomeActivity.java
@@ -17,13 +17,23 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private String TAG = "WelcomeActivity";
     private static Context context;
-
     private static LinearLayout layout;
+    private static String mois;
+    private static Date dateEventsfinal;
+
 
 
     @Override
@@ -65,79 +75,135 @@ public class WelcomeActivity extends AppCompatActivity {
             //on récupère le nom du raid de cet objet
             JsonElement nom = raidVisible.get("nom");
 
-            //on convertit en String
-            //String date = raidVisible.get("date").toString().replace("\"", " ").substring(0, 11);
 
-            String dateY=raidVisible.get("date").toString().replace("\"", " ").substring(1,5);
-            String dateM=raidVisible.get("date").toString().replace("\"", " ").substring(6,8);
-            String dateD=raidVisible.get("date").toString().replace("\"", " ").substring(9,11);
-            String date=dateD+'/'+dateM+'/'+dateY;
+            //format de la date pour comparer les évenements
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-            String nomRaid = nom.getAsString();
+            // récupération de la date du jour
+            Date today = Calendar.getInstance().getTime();
 
-            //création du visuel
-            TextView text = new TextView(context);
-            Button btn = new Button(context);
+            //préparation de la date au format yyyy-MM-dd
+            String dateY1=raidVisible.get("date").toString().replace("\"", " ").substring(1,5);
+            String dateM1=raidVisible.get("date").toString().replace("\"", " ").substring(6,8);
+            String dateD1=raidVisible.get("date").toString().replace("\"", " ").substring(9,11);
+            String dateEventstest= dateY1+"-"+dateM1+"-"+dateD1;
 
-            //ajout d'un id au bouton
-            btn.setId(k);
-
-
-
-            //ajout du texte
-            text.setText(nomRaid + '\n'+"Rejoignez l'aventure"+'\n'+date);
-            text.setX(20);
-            btn.setText("Rejoins-nous ! ");
-
-            //choix de la couleur du texte
-            text.setTextColor(context.getResources().getColor(R.color.black));
-
-            //btn.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-             //btn.setGravity(Gravity.END);
-
-             btn.setX(200);
-             btn.setBackgroundResource(R.color.black);
-
-             btn.setTextColor(context.getResources().getColor(R.color.Blancnacre));
-            btn.getBackground().setAlpha(100);
-            //btn.setHeight(150);
-            //btn.setBackgroundColor(80000000);
-
-           // lp.setMargins(left, top, right, bottom);
+            //parser la date string au format date
+            try {
+                 dateEventsfinal=df.parse(dateEventstest);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            //comparaison de la date du jour avec celle de l'évènement (le cas où l'evenement est trop vieux)
+            if (today.compareTo(dateEventsfinal)>0){
+                Utils.debug("dateEventinf",today.toString()+">"+dateEventsfinal.toString());
 
 
-
-            // Create LinearLayout
-            LinearLayout ll = new LinearLayout(context);
-
-
-
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(200, 150);
-
-            ll.addView(text);
-            ll.addView(btn,layoutParams);
+            }
+            //comparaison de la date du jour avec celle de l'évènement (cas où tout est bon)
+            else if (today.compareTo(dateEventsfinal)<0){
+                Utils.debug("dateEventsup",today.toString()+"<"+dateEventsfinal.toString());
+                String dateY=raidVisible.get("date").toString().replace("\"", " ").substring(1,5);
+                String dateM=raidVisible.get("date").toString().replace("\"", " ").substring(6,8);
+                String dateD=raidVisible.get("date").toString().replace("\"", " ").substring(9,11);
 
 
-            ll.setBackgroundResource(R.drawable.coureur2);
-            ll.getBackground().setAlpha(150);
+                if (dateD.contains("0")) {
+                    dateD = dateD.replace("0", " ");
+                }
 
-            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)layout.getLayoutParams();
-            params.setMargins(30, 0, 35, 0); //substitute parameters for left, top, right, bottom
-            layout.setLayoutParams(params);
+                HashMap<String,String> map = new HashMap<>();
+                map.put("01","Janvier");
+                map.put("02","Février");
+                map.put("03","Mars");
+                map.put("04","Avril");
+                map.put("05","Mai");
+                map.put("06","Juin");
+                map.put("07","Juillet");
+                map.put("08","Aout");
+                map.put("09","Septembre");
+                map.put("10","Octobre");
+                map.put("11","Novembre");
+                map.put("12","Décembre");
 
-            layout.addView(ll);
-            //layout.getLayoutParams();
+                for (Map.Entry months : map.entrySet()){
+                    if (months.getKey().equals(dateM)){
+                        mois = months.getValue().toString();
+                    }
+                }
 
-            //on affiche la page de connexion si on appuie sur le rejoins -nous
-              btn.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Intent intent = new Intent(context, Accueil.class);
-                  intent.putExtra("Classname","Welcome");
-                  context.startActivity(intent);
-              }
-          });;
+                String date=dateD+' '+mois+' '+dateY;
+                //String date=dateD+'/'+dateM+'/'+dateY;
+
+                String nomRaid = nom.getAsString();
+
+                //création du visuel
+                TextView text = new TextView(context);
+                Button btn = new Button(context);
+
+                //ajout d'un id au bouton
+                btn.setId(k);
+
+
+
+                //ajout du texte
+                text.setText(nomRaid + '\n'+"Rejoignez l'aventure"+'\n'+"le "+date);
+                text.setX(20);
+                btn.setText("Rejoins-nous ! ");
+
+                //choix de la couleur du texte
+                text.setTextColor(context.getResources().getColor(R.color.black));
+
+                //btn.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+                //btn.setGravity(Gravity.END);
+
+                btn.setX(200);
+                btn.setBackgroundResource(R.color.black);
+
+                btn.setTextColor(context.getResources().getColor(R.color.Blancnacre));
+                btn.getBackground().setAlpha(100);
+                //btn.setHeight(150);
+                //btn.setBackgroundColor(80000000);
+
+                // lp.setMargins(left, top, right, bottom);
+
+
+
+                // Create LinearLayout
+                LinearLayout ll = new LinearLayout(context);
+
+
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(200, 150);
+
+                ll.addView(text);
+                ll.addView(btn,layoutParams);
+
+
+                ll.setBackgroundResource(R.drawable.coureur2);
+                ll.getBackground().setAlpha(150);
+
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)layout.getLayoutParams();
+                params.setMargins(30, 0, 35, 0); //substitute parameters for left, top, right, bottom
+                layout.setLayoutParams(params);
+
+                layout.addView(ll);
+                //layout.getLayoutParams();
+
+                //on affiche la page de connexion si on appuie sur le rejoins -nous
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(context, Accueil.class);
+                        intent.putExtra("Classname","Welcome");
+                        context.startActivity(intent);
+                    }
+                });
+            }
+
+
+
         }
 
     }
